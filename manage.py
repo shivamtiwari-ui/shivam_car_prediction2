ad8d00e58f1c33ccebde954fb4db577b79b6e098
shivam <shivam96ti@gmail.com>
2024-08-26 21:13:11 +0530

    markdown ass

--- manage.py
+++ manage.py
@@ -1,11 +1,31 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+# #!/usr/bin/env python
+# """Django's command-line utility for administrative tasks."""
+# import os
+# import sys
+
+
+# def main():
+#     """Run administrative tasks."""
+#     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+#     try:
+#         from django.core.management import execute_from_command_line
+#     except ImportError as exc:
+#         raise ImportError(
+#             "Couldn't import Django. Are you sure it's installed and "
+#             "available on your PYTHONPATH environment variable? Did you "
+#             "forget to activate a virtual environment?"
+#         ) from exc
+#     execute_from_command_line(sys.argv)
+
+
+# if __name__ == '__main__':
+#     main()
+
+
 import os
 import sys
 
-
 def main():
-    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -15,8 +35,9 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
-
+    # Use the PORT environment variable from Render if available
+    port = os.environ.get('PORT', '8000')
+    execute_from_command_line([sys.argv[0], 'runserver', '0.0.0.0:' + port])
 
 if __name__ == '__main__':
     main()
